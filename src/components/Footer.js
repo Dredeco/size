ed57d1f3d9958b2b995418b514628ab00f470fd9
André Fernandes <andrefersouza@gmail.com>
2023-04-05 17:08:11 -0300

    Criação do layout geral e da página inicial

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <span>
+      SIZE © 2023 | Todos os direitos reservados <br /> Criação de Sites por: <a href='https://dredeco.github.io/portfolio/'>André Fernandes</a>
+      </span>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from 'next/legacy/image'
+import Link from 'next/link'
+
+import Logo from './../../public/logo.png'
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <Link class="navbar-brand" href="/">
+                <Image 
+                    className='logo'
+                    src={Logo}
+                    width='65'
+                    height='50'
+                />
+            </Link>
+
+            <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <Link class="nav-link active" aria-current="page" href="/">Início</Link>
+                        <Link class="nav-link" href="/Sobre">Sobre</Link>
+                        <Link class="nav-link" href="/Projetos">Projetos</Link>
+                        <Link class="nav-link" href="/Contato">Contato</Link>
+                    </div>
+                </div>
+            </div>
+    </nav>
+  )
+}
