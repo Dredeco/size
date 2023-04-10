bf9948b40277674d3bfbf34686738885fab34d33
dredeco <andrefersouza@gmail.com>
2023-04-10 18:00:36 -0300

    atualizada a pagina de Sobre

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -27,10 +27,10 @@ export default function Contato() {
           <div className='contact-info'>
               <h2>Informações</h2>
               <FaPhone /><strong>Telefone:</strong><p>(22) XXXXX-XXXX</p><br />
-              <GrMail /><strong>E-mail:</strong><p>illuminandes@iluminandes.com.br</p><br />
-              <FaMapMarkerAlt /><strong>Localização:</strong><p>Rua Av. 28 de Março, nº000 - Sala 3</p><br />
+              <GrMail /><strong>E-mail:</strong><p>comercialsize@gmail.com</p><br />
+              <FaMapMarkerAlt /><strong>Localização:</strong><p>Rua Abílio Moreira de Miranda Nº 45, sala 1002 - Imbetiba, Macaé/RJ</p><br />
               <FaClock /><strong>Horário de Atendimento:</strong><p>De segunda a sexta, das 9h às 17h.</p><br />
-              <strong>Redes sociais</strong><p><Link href='/'><FaFacebook /></Link><Link href='/'><FaInstagram /></Link></p><br />
+              <strong>Redes sociais</strong><p><Link href='https://facebook.com' target='_blank'><FaFacebook /></Link><Link href='https://instagram.com' target='_blank'><FaInstagram /></Link></p><br />
           </div>
 
       </div>
--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -5,50 +5,63 @@ import Image from 'next/legacy/image'
 import banner from './../../public/mecanico.jpg'
 
 export default function Sobre() {
+  let pilares = [
+    {name: "Conhecimento técnico", description: "Visamos fornecer a nossos clientes conhecimento especializado e experiência em áreas como projeto, construção e fabricação"},
+    {name: "Soluções de alta qualidade", description: "Nos esforçamos para fornecer soluções de alta qualidade que atendam às necessidades de nossos clientes, respeitando os padrões e regulamentos do setor."},
+    {name: "Segurança e confiabilidade ", description: "Trabalhamos para garantir que as soluções sejam seguras e confiáveis e que cumpram todos os padrões e regulamentos de segurança relevantes."},
+    {name: "Aumente a eficiência e a produtividade ", description: "Otimizamos os processos de fabricação e/ou projetamos produtos mais eficientes para ajudar os clientes a reduzir custos, aumentar a produtividade e melhorar seus resultados."},
+    {name: "Prazos e orçamentos do projeto ", description: "Nos desafiamos em concluir os projetos no prazo e dentro do orçamento, ao mesmo tempo em que oferecemos soluções de alta qualidade que atendem às necessidades do cliente."}
+  ]
+
+  let valores = [
+    {name: "Integridade", description: "Valorizamos a integridade e o compromisso de agir com ética e honestidade em todos os nossos negócios."},
+    {name: "Excelência", description: "Valorizamos a excelência e comprometimento em fornecer soluções de alta qualidade que atendam ou superem as expectativas do cliente."},
+    {name: "Colaboração", description: "Valorizamos o trabalho em estreita colaboração com nossos clientes e funcionários para promover uma cultura de trabalho em equipe e cooperação."},
+    {name: "Inovação", description: "Valorizamos a inovação e nos empenhamos em permanecer na vanguarda das novas tecnologias e práticas de engenharia."},
+    {name: "Segurança", description: "Valorizamos a segurança nos empenhamos em garantir que nossas soluções sejam projetadas e construídas com os mais altos padrões de segurança em mente."},
+    {name: "Sustentabilidade", description: "Valorizamos a sustentabilidade e nos comprometemos em projetar soluções que sejam ecologicamente corretas e socialmente responsáveis."}
+  ]
+
   return (
     <div className='about'>
       <div className='container'>
         <h1>Sobre a Size</h1>
-        <div className='about-wrapper'>
-        <div className='about-image'>
-        <Image 
-          src={banner}
-          alt=''
-          layout='fixed'
-          width={300}
-          height={300}
-        />
-        </div>
 
         <div className='about-text'>
-        <span >
-          Visando agregar valores,
-          soluções e agilidade na
-          execução das suas
-          demandas, atuamos com
-          foco na área de engenharia
-          de projetos, desde a sua
-          concepção de análise,
-          transcorrendo pela
-          elaboração de
-          documentações e cálculos,
-          até a sua execução, com
-          acompanhamento
-          especializado de nossos
-          engenheiros e responsáveis
-          técnicos.
-          A SIZE dispõe em seu corpo
-          técnico, profissionais
-          capacitados, com uma visão
-          renovada do mercado, sem
-          vícios, buscando quebrar os
-          padrões e apresentar um
-          trabalho personalizado para
-          cada um de seus clientes.
-        </span><br />
-        <Link href="/Projetos">Veja os nossos projetos</Link>
+            <span >
+            A SIZE é uma empresa que visa fornecer serviços técnicos especializados em engenharia e soluções em variados setores da indústria. <br /><br />
+            Nos propomos a oferecer serviços como design de produtos, design de processos de fabricação, gerenciamento de projetos, gerenciamento de construção e consultoria. <br /><br />
+            <strong>Nosso principal objetivo é ajudar os clientes a atingir seus objetivos, nos apoiando nos 5 pilares, que são:</strong>
+            </span><br />
         </div>
+
+        <div className='about-pilars'>
+          {pilares.map((pilar) => (
+            <div key={pilar.name} className='about-pilar-card'>
+              <h3>{pilar.name}</h3>
+              <span>{pilar.description}</span>
+            </div>
+          ))}
         </div>
+
+        <div className='about-values'>
+            <h1>Valores</h1>
+            <span>Nos valoramos em princípios que moldam como conduzimos nossos negócios e interação com nossos clientes, funcionários e a comunidade em geral. Tais quais:</span>
+            <div className='values-wrapper'>
+              {valores.map((valor) => (
+                <div key={valor.name} className='value-card'>
+                  <h4>{valor.name}</h4>
+                  <span>{valor.description}</span>
+                </div>
+              ))}
+            </div>
+        </div>
+
+        <div className='cta'>
+            <span>Ficou interessado em nossos serviços?</span>
+            <Link href='/Projetos'>VEJA AQUI NOSSOS PROJETOS</Link>
+        </div>
+
       </div>
     </div>
   )
