27b58bff961f20545e6b611ea597d45efc79b997
Dredeco <86372171+Dredeco@users.noreply.github.com>
2023-04-28 16:43:59 -0300

    atualização do layout da pagina de serviços

--- src/pages/Servicos.js
+++ src/pages/Servicos.js
@@ -2,10 +2,23 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function Servicos() {
+  let space = <br />
   let services = [
-    {data: 'flush-collapseOne', name: "Modelagem 3D", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."},
-    {data: 'flush-collapseTwo',name: "Dimensionamento e Cálculo Estrutural", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."},
-    {data: 'flush-collapseThree',name: "Laudos e ART", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."},
+    {data: 'flush-collapseOne', name: "Modelagem 3D", description: <p>A modelagem 3D é o processo de criação de objetos virtuais em três dimensões usando software especializado.<br /><br />
+    Esses objetos podem ser usados em muitas indústrias, como design de produtos, cinema e jogos de vídeo. O processo de modelagem envolve a criação de formas básicas e adição de detalhes, como texturas e iluminação, para tornar o objeto mais realista.<br /><br />
+    Depois que o modelo 3D é criado, ele pode ser exportado para diferentes formatos de arquivo, dependendo de como será usado.</p>},
+
+    {data: 'flush-collapseTwo',name: "Dimensionamento e Cálculo Estrutural", description:<p>O dimensionamento e cálculo estrutural é um serviço que envolve a análise e dimensionamento de estruturas para garantir que sejam fortes o suficiente para suportar cargas e condições ambientais específicas. <br /><br />
+    
+    É frequentemente usado em construção civil, arquitetura e engenharia civil para projetar diversas estruturas. Os profissionais usam software especializado para analisar o comportamento da estrutura em diferentes condições e determinar os materiais adequados para a construção. <br /><br />
+
+    O resultado é um projeto estrutural detalhado que inclui especificações precisas de materiais, dimensões e outras informações necessárias para a construção da estrutura com segurança e eficiência.</p>},
+
+    {data: 'flush-collapseThree',name: "Documentações e Laudos de SST", description: <p>O serviço é oferecido para garantir que as empresas cumpram as leis e regulamentações de saúde e segurança do trabalho.<br /><br /> 
+      
+    Profissionais de segurança do trabalho prestam esse serviço, que inclui análise de riscos e perigos associados às atividades laborais e identificação de medidas preventivas para reduzir esses riscos.<br /><br /> A documentação gerada inclui laudos técnicos e relatórios de inspeções de segurança do trabalho, avaliações de riscos e medidas preventivas necessárias. <br /><br />
+    
+    Esses documentos são importantes para cumprir exigências legais e regulatórias de saúde e segurança do trabalho e garantir que os trabalhadores tenham um ambiente de trabalho seguro e saudável. Eles também podem ser usados para fins de auditoria, investigação de acidentes e para garantir a conformidade com normas e regulamentações relevantes.</p>},
     {data: 'flush-collapseFour',name: "PMOC - Sistemas de HVAC e Ar Condicionado", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."},
     {data: 'flush-collapseFive',name: "Gerenciamento de Cronograma Executivo", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."},
     {data: 'flush-collapseSix',name: "Levantamento de Riscos Ocupacionais", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac."}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Image from 'next/legacy/image'
+import Link from 'next/link'
 import {FaRegCircle, FaChevronLeft, FaChevronRight} from 'react-icons/fa'
 
 import Image3d from './../../public/3dmodel.jpg'
@@ -7,9 +8,9 @@ import sms from './../../public/sms.png'
 import worker from './../../public/worker1.jpg'
 
 let sliders = [
-  {name: 'Dimensionamento e cálculo estrutural', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac.', image: desenho},
-  {name: 'Documentações e Laudos de Saúde e Segurança do Trabalho', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac.', image: sms},
-  {name: 'Caldeiraria e Usinagem', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac.', image: worker},
+  {name: 'Dimensionamento e cálculo estrutural', description: 'Serviço que envolve a análise e dimensionamento de estruturas para garantir que elas sejam fortes o suficiente para suportar cargas e condições ambientais específicas.', image: desenho},
+  {name: 'Documentações e Laudos de Saúde e Segurança do Trabalho', description: 'Serviço oferecido para garantir que as empresas cumpram as leis e regulamentações de saúde e segurança do trabalho.', image: sms},
+  {name: 'Caldeiraria e Usinagem', description: 'O serviço de caldeiraria e usinagem é oferecido para produzir e reparar peças e estruturas metálicas, utilizando técnicas de corte, dobra, solda e usinagem.', image: worker},
 ]
 
 export default function Home() {
@@ -27,18 +28,22 @@ export default function Home() {
             <div class="carousel-inner">
                   <div class="carousel-item active">
                   <div className='text'>
+                  <Link href='/Servicos'>
                     <h1>Modelagem 3D</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac.</p>
+                    <p>A modelagem 3D é o processo de criação de objetos virtuais em três dimensões usando software especializado.</p>
+                  </Link>
                   </div>
                   <Image src={Image3d} layout='fill' objectFit='cover' alt="First slide" />
                   </div>
                   {sliders.map((slider) => (
                     <>
                       <div class="carousel-item" key={slider.name}>
-                      <div className='text'>
-                        <h1>{slider.name}</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum tincidunt. Sed suscipit mollis metus. Cras dictum dui erat, ac fermentum tortor cursus ac.</p>
-                      </div>
+                          <div className='text'>
+                          <Link href='/Servicos'>
+                            <h1>{slider.name}</h1>
+                            <p>{slider.description}</p>
+                          </Link>
+                          </div>
                       <Image src={slider.image} layout='fill' objectFit='cover' alt={slider.name} />
                       </div>
                     </>
